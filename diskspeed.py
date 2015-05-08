4d71e5c773d483cd467547d4f8f357ecd79dfce8
sanderjo <sander.jonkers+github@gmail.com>
2015-05-08 22:34:28 +0200

    cleaning up

--- diskspeed.py
+++ diskspeed.py
@@ -2,14 +2,14 @@
 
 import time, os, sys
 
-def writetofile(filename,mysize):
+def writetofile(filename,mysizeMB):
+	# writes string to specified file repeatdely, until mysizeMB is reached. Then deletes fle 
 	mystring = "The quick brown fox jumps over the lazy dog"
-	writeloops = int(mysize/len(mystring))
-
+	writeloops = int(1000000*mysizeMB/len(mystring))
 	try:
 		f = open(filename, 'w')
 	except:
-		print "Error writing"
+		# no better idea than:
 		raise
 	for x in range(0, writeloops):
 		f.write(mystring)
@@ -18,51 +18,53 @@ def writetofile(filename,mysize):
 
 ############## 
 
-
-def diskspeedmeasure(fileordirname):
-	filename = 'outputTESTING.txt'	# default filename
-	filesize = 1000000
-	maxtime = 0.5 # in sec
-
-	if os.path.isdir(fileordirname): 
-		filename = os.path.join(fileordirname,filename)
-	elif os.path.isfile(fileordirname):
-		filename = fileordirname
-	else:
-		filename = fileordirname
-
+def diskspeedmeasure(dirname):
+	# returns writing speed to dirname in MB/s
+	# method: keep writing a file, until 0.5 seconds is passed. Then divide bytes written by time passed
+	filesize = 1	# in MB
+	maxtime = 0.5 	# in sec
+	filename = os.path.join(dirname,'outputTESTING.txt')
 	start = time.time()
 	loopcounter = 0
 	while True:
 		try:
 			writetofile(filename, filesize)
 		except:
-			print "EERRRROOROROR ... bailing out"
-			sys.exit(0)
+			# I have no better idea than:
+			raise	
 		loopcounter += 1
-		now = time.time()
-		diff = now-start
+		diff = time.time() - start
 		if diff > maxtime: break
-
-	return (loopcounter*filesize/1000000)/diff
+	return (loopcounter*filesize)/diff
 
 ############## Start of main
 
 
 if __name__ == "__main__":
+
 	print "Let's go"
-	filename = 'outputTESTING.txt'	# default filename
+
 	if len(sys.argv) >= 2:
-		arg1 = sys.argv[1]
-		if os.path.isdir(arg1): 
-			filename = os.path.join(arg1,filename)
-		elif os.path.isfile(arg1):
-			filename = arg1
-		else:
-			print "Specified argument is not a filename nor a directory. Bailing out"
+		dirname = sys.argv[1]
+		if not os.path.isdir(dirname): 
+			print "Specified argument is not a directory. Bailing out"
 			sys.exit(1)
-	print "filename", filename
-	print "Disk writing speed:", diskspeedmeasure(filename), "Mbytes per second"
+	else:
+		# no argument, so use current working directory
+		dirname = os.getcwd()
+		print "Using current working directory"
+
+	try:
+		speed = diskspeedmeasure(dirname)
+		print("Disk writing speed: %.2f Mbytes per second" % speed)
+	except IOError, e:
+		#print "IOError:", e
+		if e.errno == 13:
+			print "Could not create test file. Check that you have write rights to directory", dirname
+	except:
+		print "Something else went wrong"
+		raise
+
 	print "Done"
 
 
