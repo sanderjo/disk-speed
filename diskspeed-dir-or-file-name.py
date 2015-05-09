3389174c51e578da2ae68a7fb0b341574df5fa39
sanderjo <sander.jonkers+github@gmail.com>
2015-05-09 22:01:15 +0200

    cleaning up

---
+++ diskspeed-dir-or-file-name.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import time, os, sys
+
+def writetofile(filename,mysize):
+	mystring = "The quick brown fox jumps over the lazy dog"
+	writeloops = int(1000000*mysize/len(mystring))
+
+	try:
+		f = open(filename, 'w')
+	except:
+		print "Error writing"
+		raise
+	for x in range(0, writeloops):
+		f.write(mystring)
+	f.close()
+	os.remove(filename)
+
+############## 
+
+
+def diskspeedmeasure(fileordirname):
+	filename = 'outputTESTING.txt'	# default filename
+	filesize = 1	# in MB
+	maxtime = 0.5 # in sec
+
+	if os.path.isdir(fileordirname): 
+		filename = os.path.join(fileordirname,filename)
+	elif os.path.isfile(fileordirname):
+		filename = fileordirname
+	else:
+		filename = fileordirname
+
+	start = time.time()
+	loopcounter = 0
+	while True:
+		try:
+			writetofile(filename, filesize)
+		except:
+			print "EERRRROOROROR ... bailing out"
+			sys.exit(0)
+		loopcounter += 1
+		#now = time.time()
+		diff = time.time() - start
+		if diff > maxtime: break
+
+	return (loopcounter*filesize)/diff
+
+############## Start of main
+
+
+if __name__ == "__main__":
+	print "Let's go"
+	filename = 'outputTESTING.txt'	# default filename
+	if len(sys.argv) >= 2:
+		arg1 = sys.argv[1]
+		if os.path.isdir(arg1): 
+			filename = os.path.join(arg1,filename)
+		elif os.path.isfile(arg1):
+			filename = arg1
+		else:
+			print "Specified argument is not a filename nor a directory. Bailing out"
+			sys.exit(1)
+	print "filename", filename
+	print "Disk writing speed:", diskspeedmeasure(filename), "Mbytes per second"
+	print "Done"
+
+
--- example-on-tmp-dir.py
+++ example-on-tmp-dir.py
@@ -4,6 +4,4 @@ import diskspeed
 
 print diskspeed.diskspeedmeasure('/tmp')
 print diskspeed.diskspeedmeasure('/tmp/')
-print diskspeed.diskspeedmeasure('/tmp/blabla.txt')
-print diskspeed.diskspeedmeasure('~')
 
